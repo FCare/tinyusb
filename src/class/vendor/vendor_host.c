8fc2728506dfaa8c17d2869d0cabfb27ead25b86
Francois CARON <francois.caron@renault.com>
2022-02-16 17:10:34 +0100

    Add Xbox 360 clone gamepad in the vendor specific interface

--- src/class/vendor/vendor_host.c
+++ src/class/vendor/vendor_host.c
@@ -32,115 +32,216 @@
 // INCLUDE
 //--------------------------------------------------------------------+
 #include "host/usbh.h"
+#include "host/usbh_classdriver.h"
+
 #include "vendor_host.h"
 
 //--------------------------------------------------------------------+
 // MACRO CONSTANT TYPEDEF
 //--------------------------------------------------------------------+
 
+
+typedef struct
+{
+  uint8_t itf_num;
+  uint8_t ep_in;
+  uint8_t ep_out;
+
+  uint16_t epin_size;
+  uint16_t epout_size;
+
+  uint8_t epin_buf[CFG_TUH_VENDOR_EPIN_BUFSIZE];
+  uint8_t epout_buf[CFG_TUH_VENDOR_EPOUT_BUFSIZE];
+} vendorh_interface_t;
+
+
+typedef struct
+{
+  uint8_t inst_count;
+  vendorh_interface_t instances[CFG_TUH_VENDOR];
+} vendorh_device_t;
+
 //--------------------------------------------------------------------+
 // INTERNAL OBJECT & FUNCTION DECLARATION
 //--------------------------------------------------------------------+
-custom_interface_info_t custom_interface[CFG_TUH_DEVICE_MAX];
 
-static tusb_error_t cush_validate_paras(uint8_t dev_addr, uint16_t vendor_id, uint16_t product_id, void * p_buffer, uint16_t length)
-{
-  if ( !tusbh_custom_is_mounted(dev_addr, vendor_id, product_id) )
-  {
-    return TUSB_ERROR_DEVICE_NOT_READY;
-  }
+static vendorh_device_t _vendorh_dev[CFG_TUH_DEVICE_MAX];
 
-  TU_ASSERT( p_buffer != NULL && length != 0, TUSB_ERROR_INVALID_PARA);
-
-  return TUSB_ERROR_NONE;
-}
-//--------------------------------------------------------------------+
-// APPLICATION API (need to check parameters)
-//--------------------------------------------------------------------+
-tusb_error_t tusbh_custom_read(uint8_t dev_addr, uint16_t vendor_id, uint16_t product_id, void * p_buffer, uint16_t length)
-{
-  TU_ASSERT_ERR( cush_validate_paras(dev_addr, vendor_id, product_id, p_buffer, length) );
-
-  if ( !hcd_pipe_is_idle(custom_interface[dev_addr-1].pipe_in) )
-  {
-    return TUSB_ERROR_INTERFACE_IS_BUSY;
-  }
-
-  (void) usbh_edpt_xfer( custom_interface[dev_addr-1].pipe_in, p_buffer, length);
-
-  return TUSB_ERROR_NONE;
-}
-
-tusb_error_t tusbh_custom_write(uint8_t dev_addr, uint16_t vendor_id, uint16_t product_id, void const * p_data, uint16_t length)
-{
-  TU_ASSERT_ERR( cush_validate_paras(dev_addr, vendor_id, product_id, p_data, length) );
-
-  if ( !hcd_pipe_is_idle(custom_interface[dev_addr-1].pipe_out) )
-  {
-    return TUSB_ERROR_INTERFACE_IS_BUSY;
-  }
-
-  (void) usbh_edpt_xfer( custom_interface[dev_addr-1].pipe_out, p_data, length);
-
-  return TUSB_ERROR_NONE;
-}
+TU_ATTR_ALWAYS_INLINE static inline vendorh_device_t* get_dev(uint8_t dev_addr);
+TU_ATTR_ALWAYS_INLINE static inline vendorh_interface_t* get_instance(uint8_t dev_addr, uint8_t instance);
+static uint8_t get_instance_id_by_itfnum(uint8_t dev_addr, uint8_t itf);
+static uint8_t get_instance_id_by_epaddr(uint8_t dev_addr, uint8_t ep_addr);
 
 //--------------------------------------------------------------------+
 // USBH-CLASS API
 //--------------------------------------------------------------------+
-void cush_init(void)
+
+
+static void config_driver_mount_complete(uint8_t dev_addr, uint8_t instance, uint8_t const* desc_report, uint16_t desc_len);
+
+void vendorh_init(void)
 {
-  tu_memclr(&custom_interface, sizeof(custom_interface_info_t) * CFG_TUH_DEVICE_MAX);
+  tu_memclr(_vendorh_dev, sizeof(_vendorh_dev));
 }
 
-tusb_error_t cush_open_subtask(uint8_t dev_addr, tusb_desc_interface_t const *p_interface_desc, uint16_t *p_length)
+bool vendorh_open(uint8_t rhport, uint8_t dev_addr, tusb_desc_interface_t const *desc_itf, uint16_t max_len)
 {
-  // FIXME quick hack to test lpc1k custom class with 2 bulk endpoints
-  uint8_t const *p_desc = (uint8_t const *) p_interface_desc;
-  p_desc = tu_desc_next(p_desc);
 
-  //------------- Bulk Endpoints Descriptor -------------//
-  for(uint32_t i=0; i<2; i++)
-  {
-    tusb_desc_endpoint_t const *p_endpoint = (tusb_desc_endpoint_t const *) p_desc;
-    TU_ASSERT(TUSB_DESC_ENDPOINT == p_endpoint->bDescriptorType, TUSB_ERROR_INVALID_PARA);
+  int nbEndpoint = 0;
 
-    pipe_handle_t * p_pipe_hdl =  ( p_endpoint->bEndpointAddress &  TUSB_DIR_IN_MASK ) ?
-                         &custom_interface[dev_addr-1].pipe_in : &custom_interface[dev_addr-1].pipe_out;
-    *p_pipe_hdl = usbh_edpt_open(dev_addr, p_endpoint, TUSB_CLASS_VENDOR_SPECIFIC);
-    TU_ASSERT ( pipehandle_is_valid(*p_pipe_hdl), TUSB_ERROR_HCD_OPEN_PIPE_FAILED );
+  //ONLY SUPPORT xbox 360 gamepad interface as defined in https://github.com/torvalds/linux/blob/master/drivers/input/joystick/xpad.c
+  TU_VERIFY(desc_itf->bInterfaceClass == TUSB_CLASS_VENDOR_SPECIFIC);
+  TU_VERIFY(desc_itf->bInterfaceSubClass == 93);
+  TU_VERIFY(desc_itf->bInterfaceProtocol == 129);
 
+  TU_LOG1("VENDOR opening Interface %u (addr = %u) (endPoints %u)\r\n", desc_itf->bInterfaceNumber, dev_addr, desc_itf->bNumEndpoints);
+
+  uint16_t const drv_len = sizeof(tusb_desc_interface_t) +  desc_itf->bNumEndpoints*sizeof(tusb_desc_endpoint_t);
+  TU_ASSERT(max_len >= drv_len);
+  TU_ASSERT(desc_itf->bNumEndpoints == 2);
+  nbEndpoint = desc_itf->bNumEndpoints;
+
+  uint8_t const *p_desc = (uint8_t const *) desc_itf;
+
+  vendorh_device_t* vendor_dev = get_dev(dev_addr);
+  TU_ASSERT(vendor_dev->inst_count < CFG_TUH_VENDOR, 0);
+
+  vendorh_interface_t* vendor_itf = get_instance(dev_addr, vendor_dev->inst_count);
+  vendor_dev->inst_count++;
+  vendor_itf->itf_num   = desc_itf->bInterfaceNumber;
+
+  while (nbEndpoint != 0) {
     p_desc = tu_desc_next(p_desc);
-  }
+    tusb_desc_endpoint_t const * desc_ep = (tusb_desc_endpoint_t const *) p_desc;
+    TU_LOG1("VENDOR Endpoint %u type %u input %u size %u\r\n", nbEndpoint, desc_ep->bDescriptorType, tu_edpt_dir(desc_ep->bEndpointAddress), desc_ep->wMaxPacketSize.size);
+    if(TUSB_DESC_ENDPOINT == desc_ep->bDescriptorType) {
+      nbEndpoint--;
+        TU_ASSERT( usbh_edpt_open(rhport, dev_addr, desc_ep) );
 
-  (*p_length) = sizeof(tusb_desc_interface_t) + 2*sizeof(tusb_desc_endpoint_t);
-  return TUSB_ERROR_NONE;
+        if(tu_edpt_dir(desc_ep->bEndpointAddress) == TUSB_DIR_OUT) {
+          vendor_itf->ep_out     = desc_ep->bEndpointAddress;
+          vendor_itf->epout_size = desc_ep->wMaxPacketSize.size;
+        }
+        else {
+          vendor_itf->ep_in     = desc_ep->bEndpointAddress;
+          vendor_itf->epin_size = desc_ep->wMaxPacketSize.size;
+        }
+    }
+  }
+return true;
 }
 
-void cush_isr(pipe_handle_t pipe_hdl, xfer_result_t event)
-{
 
+bool vendorh_set_config(uint8_t dev_addr, uint8_t itf_num) {
+
+  uint8_t const instance    = get_instance_id_by_itfnum(dev_addr, itf_num);
+  vendorh_interface_t* vendor_itf = get_instance(dev_addr, instance);
+
+  TU_LOG1("VENDOR SET CONFIG %u  %u\r\n", dev_addr, itf_num);
+  
+  config_driver_mount_complete(dev_addr, instance, NULL, 0);
+
+  return true;
 }
 
-void cush_close(uint8_t dev_addr)
+
+bool tuh_vendor_receive_report(uint8_t dev_addr, uint8_t instance)
 {
-  tusb_error_t err1, err2;
-  custom_interface_info_t * p_interface = &custom_interface[dev_addr-1];
+  vendorh_interface_t* vendor_itf = get_instance(dev_addr, instance);
+  TU_LOG1("VENDOR RECEIVE REPORT %u\r\n", dev_addr);
+  // claim endpoint
+  TU_VERIFY( usbh_edpt_claim(dev_addr, vendor_itf->ep_in) );
 
-  // TODO re-consider to check pipe valid before calling pipe_close
-  if( pipehandle_is_valid( p_interface->pipe_in ) )
+  return usbh_edpt_xfer(dev_addr, vendor_itf->ep_in, vendor_itf->epin_buf, vendor_itf->epin_size);
+}
+
+
+static void config_driver_mount_complete(uint8_t dev_addr, uint8_t instance, uint8_t const* desc_report, uint16_t desc_len)
+{
+  vendorh_interface_t* vendor_itf = get_instance(dev_addr, instance);
+  TU_LOG1("VENDOR MOUNT COMPLETE %u\r\n", dev_addr);
+  // enumeration is complete
+  tuh_vendor_mount_cb(dev_addr, instance, desc_report, desc_len);
+
+  // notify usbh that driver enumeration is complete
+  usbh_driver_set_config_complete(dev_addr, vendor_itf->itf_num);
+}
+
+bool vendorh_xfer_cb(uint8_t dev_addr, uint8_t ep_addr, xfer_result_t result, uint32_t xferred_bytes)
+{
+  (void) result;
+
+  uint8_t const dir = tu_edpt_dir(ep_addr);
+  uint8_t const instance = get_instance_id_by_epaddr(dev_addr, ep_addr);
+  vendorh_interface_t* vendor_itf = get_instance(dev_addr, instance);
+
+  if ( dir == TUSB_DIR_IN )
   {
-    err1 = hcd_pipe_close( p_interface->pipe_in );
+    TU_LOG1("Get Report callback (%u, %u)\r\n", dev_addr, instance);
+    tuh_vendor_report_received_cb(dev_addr, instance, vendor_itf->epin_buf, xferred_bytes);
+  }else
+  {
+    TU_LOG1("Get Report sent callback (%u, %u)\r\n", dev_addr, instance);
+    if (tuh_vendor_report_sent_cb) tuh_vendor_report_sent_cb(dev_addr, instance, vendor_itf->epout_buf, xferred_bytes);
   }
 
-  if ( pipehandle_is_valid( p_interface->pipe_out ) )
+  return true;
+}
+
+void vendorh_close(uint8_t dev_addr)
+{
+  TU_VERIFY(dev_addr <= CFG_TUH_DEVICE_MAX, );
+
+  vendorh_device_t* vendor_dev = get_dev(dev_addr);
+
+  if (tuh_vendor_umount_cb)
   {
-    err2 = hcd_pipe_close( p_interface->pipe_out );
+    for (uint8_t inst = 0; inst < vendor_dev->inst_count; inst++ ) tuh_vendor_umount_cb(dev_addr, inst);
   }
 
-  tu_memclr(p_interface, sizeof(custom_interface_info_t));
+  tu_memclr(vendor_dev, sizeof(vendorh_device_t));
+}
 
-  TU_ASSERT(err1 == TUSB_ERROR_NONE && err2 == TUSB_ERROR_NONE, (void) 0 );
+//--------------------------------------------------------------------+
+// Helper
+//--------------------------------------------------------------------+
+
+// Get Device by address
+TU_ATTR_ALWAYS_INLINE static inline vendorh_device_t* get_dev(uint8_t dev_addr)
+{
+  return &_vendorh_dev[dev_addr-1];
+}
+
+// Get Interface by instance number
+TU_ATTR_ALWAYS_INLINE static inline vendorh_interface_t* get_instance(uint8_t dev_addr, uint8_t instance)
+{
+  return &_vendorh_dev[dev_addr-1].instances[instance];
+}
+
+// Get instance ID by interface number
+static uint8_t get_instance_id_by_itfnum(uint8_t dev_addr, uint8_t itf)
+{
+  for ( uint8_t inst = 0; inst < CFG_TUH_HID; inst++ )
+  {
+    vendorh_interface_t *vendor = get_instance(dev_addr, inst);
+
+    if ( (vendor->itf_num == itf) && (vendor->ep_in || vendor->ep_out) ) return inst;
+  }
+
+  return 0xff;
+}
+
+// Get instance ID by endpoint address
+static uint8_t get_instance_id_by_epaddr(uint8_t dev_addr, uint8_t ep_addr)
+{
+  for ( uint8_t inst = 0; inst < CFG_TUH_HID; inst++ )
+  {
+    vendorh_interface_t *vendor = get_instance(dev_addr, inst);
+
+    if ( (ep_addr == vendor->ep_in) || ( ep_addr == vendor->ep_out) ) return inst;
+  }
+
+  return 0xff;
 }
 
 #endif
