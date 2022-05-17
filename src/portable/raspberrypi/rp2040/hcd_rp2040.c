930012dee8e7de4225bde5fa10591c45c81ea814
Francois CARON <francois.caron@renault.com>
2022-05-17 16:03:01 +0200

    Do a 2 step action on rp2040 to start a transfer - Fixing continuous block read on SCSI device

--- src/portable/raspberrypi/rp2040/hcd_rp2040.c
+++ src/portable/raspberrypi/rp2040/hcd_rp2040.c
@@ -568,12 +568,19 @@ bool hcd_edpt_xfer(uint8_t rhport, uint8_t dev_addr, uint8_t ep_addr, uint8_t * 
         // for host we have to initiate the transfer
         usb_hw->dev_addr_ctrl = (uint32_t) (dev_addr | (ep_num << USB_ADDR_ENDP_ENDPOINT_LSB));
 
-        uint32_t flags = USB_SIE_CTRL_START_TRANS_BITS | SIE_CTRL_BASE |
-                         (ep_dir ? USB_SIE_CTRL_RECEIVE_DATA_BITS : USB_SIE_CTRL_SEND_DATA_BITS);
+        uint32_t flags = SIE_CTRL_BASE |
+                 (ep_dir ? USB_SIE_CTRL_RECEIVE_DATA_BITS : USB_SIE_CTRL_SEND_DATA_BITS);
         // Set pre if we are a low speed device on full speed hub
         flags |= need_pre(dev_addr) ? USB_SIE_CTRL_PREAMBLE_EN_BITS : 0;
 
+        // Set up the hardware with all flags except the start bit
         usb_hw->sie_ctrl = flags;
+        // Now start the transaction; if you don't do this in two parts,
+        // and the host has switched direction, sometimes the transaction
+        // does not start
+        flags |= USB_SIE_CTRL_START_TRANS_BITS;
+        usb_hw->sie_ctrl = flags;
+
     }else
     {
       hw_endpoint_xfer_start(ep, buffer, buflen);
@@ -612,8 +619,12 @@ bool hcd_setup_send(uint8_t rhport, uint8_t dev_addr, uint8_t const setup_packet
     usb_hw->dev_addr_ctrl = dev_addr;
 
     // Set pre if we are a low speed device on full speed hub
-    uint32_t const flags = SIE_CTRL_BASE | USB_SIE_CTRL_SEND_SETUP_BITS | USB_SIE_CTRL_START_TRANS_BITS |
+    uint32_t flags = SIE_CTRL_BASE | USB_SIE_CTRL_SEND_SETUP_BITS |
                            (need_pre(dev_addr) ? USB_SIE_CTRL_PREAMBLE_EN_BITS : 0);
+
+    usb_hw->sie_ctrl = flags;
+
+    flags |= USB_SIE_CTRL_START_TRANS_BITS;
 
     usb_hw->sie_ctrl = flags;
 
